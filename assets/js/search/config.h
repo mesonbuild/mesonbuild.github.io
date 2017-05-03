1c793026399818b24379535436208bcaeb09c2f2
Jussi Pakkanen <jpakkane@gmail.com>
2017-05-04 00:18:56 +0300

    Bleep. Bloop. I am a bot.

--- assets/js/search/config.h
+++ assets/js/search/config.h
@@ -1,1 +1,1 @@
-urls_downloaded_cb({"urls": ["Wrap-best-practices-and-tips.html#page-description", "Configuration.html#configuration"], "token": "config.h"});
+urls_downloaded_cb({"token": "config.h", "urls": ["Wrap-best-practices-and-tips.html#page-description", "Configuration.html#configuration"]});
--- assets/js/search/main.c
+++ assets/js/search/main.c
@@ -1,1 +1,1 @@
-urls_downloaded_cb({"urls": ["Meson-design-rationale-A-proposal-for-a-better-cross-platform-build-system.html#sample-code", "Wrap-best-practices-and-tips.html#page-description", "Tutorial.html#the-humble-beginning"], "token": "main.c"});
+urls_downloaded_cb({"token": "main.c", "urls": ["Wrap-best-practices-and-tips.html#page-description", "Tutorial.html#the-humble-beginning"]});
