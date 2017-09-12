717bd853e586ea7f559b0d23928441fac2642693
Jussi Pakkanen <jpakkane@gmail.com>
2017-09-12 21:20:51 +0300

    Bleep. Bloop. I am a bot.

--- assets/js/search/meson.py
+++ assets/js/search/meson.py
@@ -1,1 +1,1 @@
-urls_downloaded_cb({"urls": ["Running-Meson.html#running-meson"], "token": "meson.py"});
+urls_downloaded_cb({"urls": ["Running-Meson.html#running-meson", "Reference-manual.html#meson-object"], "token": "meson.py"});
