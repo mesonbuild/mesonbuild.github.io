1edd913effaed8fba17f86ef563a7f23d6c5471b
Jussi Pakkanen <jpakkane@gmail.com>
2019-10-06 22:02:13 +0300

    Bleep. Bloop. I am a bot.

--- assets/js/search/run_project_tests.py
+++ assets/js/search/run_project_tests.py
@@ -1,1 +1,1 @@
-urls_downloaded_cb({"token":"run_project_tests.py","urls":[{"url":"Contributing.html#page-description","node_type":"p","context":{"gi-language":["default"]}}]});
+urls_downloaded_cb({"token":"run_project_tests.py","urls":[{"url":"Contributing.html#page-description","node_type":"p","context":{"gi-language":["default"]}},{"url":"Release-notes-for-0-52-0.html#added-only-tests-option-to-run_project_testspy","node_type":"h2","context":{"gi-language":["default"]}},{"url":"Release-notes-for-0-52-0.html#page-description","node_type":"p","context":{"gi-language":["default"]}}]});
